6d1fdaa8c3668fce2f273e43473432c47c7947f7
francisco-leal <francisco@talentprotocol.com>
2022-09-22 17:48:49 +0100

    bootstrap TVL page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
+import "primereact/resources/themes/lara-dark-purple/theme.css";   //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+import "primeflex/primeflex.css";
 import '../styles/globals.css'
+
+import PrimeReact from 'primereact/api';
+
+PrimeReact.ripple = true;
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
