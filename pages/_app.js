8d540ffd6ccd4d8acd6391ca233552aab58f81d3
francisco-leal <francisco@talentprotocol.com>
2023-05-24 14:06:32 +0100

    Add google analytics to growth dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,31 @@ import "primereact/resources/themes/lara-dark-purple/theme.css";   //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
 import "primeflex/primeflex.css";
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from "next/script";
 
 import PrimeReact from 'primereact/api';
 
 PrimeReact.ripple = true;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+    <Script id="GoogleAnalytics" strategy="lazyOnload">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+      `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
