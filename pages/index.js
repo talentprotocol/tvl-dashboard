d4f5a2bfe0fc229bf23d4ccd2a6bd8cc806d205a
francisco-leal <francisco@talentprotocol.com>
2023-08-24 15:48:20 +0100

    Update footer of TVL app

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,28 @@
-import { useState, useEffect } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
-import { ethers } from 'ethers';
-import { CeloProvider } from '@celo-tools/celo-ethers-wrapper';
 
-import StakingMigrationABI from "./abi/StakingMigration.json";
+const TERMS_HREF = "https://talentprotocol.notion.site/Terms-Conditions-ec060cb6c06e49a98f17d235d0835773";
+
+const PRIVACY_HREF = "https://talentprotocol.notion.site/Privacy-Policy-cc2b297006b54b3db1a008592302ccf5";
+
+const ABOUT = "https://talentprotocol.notion.site/Talent-Protocol-2b10dc798a3941d5814bc10dfd102c25";
+
+const BLOG = "https://blog.talentprotocol.com/";
+
+const BOUNTIES = "https://talentprotocol.notion.site/Bounties-74c27972ac554ce0b9702e8a9131cfa7";
+
+const API = "https://docs.talentprotocol.com/api";
+
+const FAQ = "https://help.talentprotocol.com/";
+
+const TALENT_PROTOCOL_DISCORD = "https://discord.com/invite/talentprotocol";
+
+const TALENT_PROTOCOL_TWITTER = "https://twitter.com/talentprotocol";
+
+const TALENT_PROTOCOL_GITHUB = "https://github.com/talentprotocol";
+
+const TALENT_PROTOCOL_TELEGRAM = "https://t.me/talentprotocol";
 
 export default function Home() {
-  const [data, setData] = useState({ celo: {}, matic: {}});
-  
-  useEffect(() => {
-    const celoProvider = new CeloProvider("https://forno.celo.org");
-    const celoContract = new ethers.Contract(
-      "0x5a6eF881E3707AAf7201dDb7c198fc94B4b12636",
-      StakingMigrationABI.abi,
-      celoProvider
-    );
-
-    const maticProvider = new CeloProvider("https://polygon-rpc.com/");
-    const maticContract = new ethers.Contract(
-      "0xEa998Ff9c0c075cD035b25095D1833E5aF0aF873",
-      StakingMigrationABI.abi,
-      maticProvider
-    );
-
-    retrieveSmartContractData(celoContract, "celo");
-    retrieveSmartContractData(maticContract, "matic");
-  },[]);
-
-  const retrieveSmartContractData = async (contract, chain) => {
-    const activeStakes = await contract.activeStakes();
-    const stakedTAL = await contract.totalTokensStaked()
-
-    setData((prev) => ({...prev, [chain]: {
-      activeStakes: ethers.utils.commify(activeStakes),
-      stakedTAL: ethers.utils.commify(ethers.utils.formatUnits(stakedTAL)).split('.')[0]
-    }}));
-  }
-
-  const tvl = () => {
-    if(!data["celo"]["stakedTAL"] || !data["matic"]["stakedTAL"]) {
-      return "loading";
-    }
-
-    const celoTAL = ethers.utils.parseUnits(data["celo"]["stakedTAL"].replaceAll(',',''), "ether");
-    const maticTAL = ethers.utils.parseUnits(data["matic"]["stakedTAL"].replaceAll(',',''), "ether");
-
-    const result = celoTAL.add(maticTAL).div(50);
-
-    return "$" + ethers.utils.commify(ethers.utils.formatUnits(result)).split('.')[0];
-  }
-
-  const totalActiveStakes = () => {
-    if(!data["celo"]["activeStakes"] || !data["matic"]["activeStakes"]) {
-      return "loading";
-    }
-
-    const celoActiveStakes = ethers.utils.parseUnits(data["celo"]["activeStakes"].replaceAll(',',''), "ether");
-    const maticActiveStakes = ethers.utils.parseUnits(data["matic"]["activeStakes"].replaceAll(',',''), "ether");
-
-    const result = celoActiveStakes.add(maticActiveStakes);
-
-    return ethers.utils.commify(ethers.utils.formatUnits(result)).split('.')[0];
-  }
-
   const menubarItems = [
     {
       label: 'Talent Protocol',
@@ -82,12 +41,6 @@ export default function Home() {
       url: "#metrics",
       textClassName: "text-primary font-bold",
       listClassName: "py-3 md:py-0 px-3"
-    },
-    {
-      label: 'Live TVL',
-      url: "#tvl",
-      textClassName: "text-primary font-bold",
-      listClassName: "pt-3 md:pt-0 px-3"
     },
   ];
 
@@ -127,129 +80,82 @@ export default function Home() {
           </div>
         </div>
         <div className="border-bottom-1 border-primary w-screen -mx-2"></div>
-        <div className="grid my-4 md:-mx-2 mx-1">
-          <div className="col-12 md:pl-6">
-            <h2 className="text-primary font-bold" id="tvl">Live TVL</h2>
-          </div>
-          <div className="col-12 md:col-6 md:pl-6 md:pr-3 md:pb-5">
-            <div className="background-primary border-primary p-3 md:p-5 border-1 border-50 border-round-3xl">
-              <div className="flex justify-content-between">
-                <div>
-                  <span className="block text-500 font-medium mb-3">Total Value Locked</span>
-                  <div className="text-900 font-medium text-xl">{tvl()}</div>
-                </div>
-                <div className="flex align-items-center justify-content-center" style={{ width: '2.5rem', height: '2.5rem' }}>
-                  <Image src="/talent-protocol.svg" alt="Talent Protocol Logo" width={32} height={32} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 md:col-6 md:pr-6 md:pl-3 md:pb-5">
-            <div className="background-primary border-primary p-3 md:p-5 border-1 border-50 border-round-3xl">
-              <div className="flex justify-content-between">
-                <div>
-                  <span className="block text-500 font-medium mb-3">Total Active Stakes</span>
-                  <div className="text-900 font-medium text-xl">{totalActiveStakes()}</div>
-                </div>
-                <div className="flex align-items-center justify-content-center" style={{ width: '2.5rem', height: '2.5rem' }}>
-                  <Image src="/talent-protocol.svg" alt="Talent Protocol Logo" width={32} height={32} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 md:col-6 md:pl-6 md:pr-3 md:pb-5">
-            <div className="background-primary border-primary p-3 md:p-5 border-1 border-50 border-round-3xl">
-              <div className="flex justify-content-between">
-                <div>
-                  <span className="block text-500 font-medium mb-3">Staked TAL (CELO)</span>
-                  <div className="text-900 font-medium text-xl">{data["celo"]["stakedTAL"] || "loading"}</div>
-                </div>
-                <div className="flex align-items-center justify-content-center" style={{ width: '2.5rem', height: '2.5rem' }}>
-                  <Image src="/celo.svg" alt="CELO Logo" width={32} height={32} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 md:col-6 md:pr-6 md:pl-3 md:pb-5">
-            <div className="background-primary border-primary p-3 md:p-5 border-1 border-50 border-round-3xl">
-              <div className="flex justify-content-between">
-                <div>
-                  <span className="block text-500 font-medium mb-3">Active Stakes (CELO)</span>
-                  <div className="text-900 font-medium text-xl">{data["celo"]["activeStakes"]}</div>
-                </div>
-                <div className="flex align-items-center justify-content-center" style={{ width: '2.5rem', height: '2.5rem' }}>
-                  <Image src="/celo.svg" alt="CELO Logo" width={32} height={32} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 md:col-6 md:pl-6 md:pr-3 md:pb-5">
-            <div className="background-primary border-primary p-3 md:p-5 border-1 border-50 border-round-3xl">
-              <div className="flex justify-content-between">
-                <div>
-                  <span className="block text-500 font-medium mb-3">Staked TAL (MATIC)</span>
-                  <div className="text-900 font-medium text-xl">{data["matic"]["stakedTAL"] || "loading"}</div>
-                </div>
-                <div className="flex align-items-center justify-content-center" style={{ width: '2.5rem', height: '2.5rem' }}>
-                  <Image src="/polygon.svg" alt="Polygon Logo" width={32} height={32} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 md:col-6 md:pr-6 md:pl-3 md:pb-5">
-            <div className="background-primary border-primary p-3 md:p-5 border-1 border-50 border-round-3xl">
-              <div className="flex justify-content-between">
-                <div>
-                  <span className="block text-500 font-medium mb-3">Active Stakes (MATIC)</span>
-                  <div className="text-900 font-medium text-xl">{data["matic"]["activeStakes"]}</div>
-                </div>
-                <div className="flex align-items-center justify-content-center" style={{ width: '2.5rem', height: '2.5rem' }}>
-                  <Image src="/polygon.svg" alt="Polygon Logo" width={32} height={32} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 flex flex-wrap">
-            <div className="col-12 md:col-6 md:pl-6 md:pr-3">
-              <p className="text-light">Staked TAL</p>
-              <p className="text-sm line-height-3">The amount of TAL being staked in each chain. This corresponds to TAL bought by users and TAL distributed as rewards already.</p>
-            </div>
-            <div className="col-12 md:col-6 md:pr-6 md:pl-3">
-              <p className="text-light">Active Stakes</p>
-              <p className="text-sm line-height-3">The amount of Talent/Supporter pairs that exist in each chain. Each pair was created through the purchase of talent tokens.</p>
-            </div>
-          </div>
-        </div>
-        <div className="border-bottom-1 border-primary w-screen -mx-2"></div>
         <footer className="flex justify-content-between p-2 md:p-6 md:-mx-2 mx-1">
           <div className="flex-column">
             <h2 className="text-light">Talent Protocol</h2>
-            <p className="line-height-3">The web3 professional community<br/>for high-potential builders.</p>
+            <p className="line-height-3">Where builders commit to goals,<br/>share their progress and find the<br/>support they need to succeed.</p>
           </div>
-          <div className="flex-column">
-            <h3 className="text-light text-right">Project</h3>
-            <ul className='flex-column justify-content-end' style={{paddingInlineStart: 0}}>
-              <li className='mt-2 text-right'>
-                <a target="_blank" rel="noreferrer" href={"https://talentprotocol.notion.site/Detailed-Token-Economics-916806c9811b4cabb54bba3a0afc3dd4"} className="text-white">
-                  <span>Tokenomics</span>
-                </a>
-              </li>
-              <li className='mt-2 text-right'>
-                <a target="_blank" rel="noreferrer" href={"https://talentprotocol.notion.site/Talent-Protocol-2b10dc798a3941d5814bc10dfd102c25"} className="text-white">
-                  <span>About</span>
-                </a>
-              </li>
-              <li className='mt-2 text-right'>
-                <a target="_blank" rel="noreferrer" href={"https://blog.talentprotocol.com/"} className="text-white">
-                  <span>Blog</span>
-                </a>
-              </li>
-              <li className='mt-2 text-right'>
-                <a target="_blank" rel="noreferrer" href={"https://help.talentprotocol.com/"} className="text-white">
-                  <span>FAQ</span>
-                </a>
-              </li>
-            </ul>
+          <div className="flex">
+            <div className="flex-column mr-6">
+              <h3 className="text-light text-right">Project</h3>
+              <ul className='flex-column justify-content-end' style={{paddingInlineStart: 0}}>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={ABOUT} className="text-white">
+                    <span>About</span>
+                  </a>
+                </li>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={BLOG} className="text-white">
+                    <span>Blog</span>
+                  </a>
+                </li>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={BOUNTIES} className="text-white">
+                    <span>Bounties</span>
+                  </a>
+                </li>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={API} className="text-white">
+                    <span>API</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-column mr-6">
+              <h3 className="text-light text-right">Help</h3>
+              <ul className='flex-column justify-content-end' style={{paddingInlineStart: 0}}>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={FAQ} className="text-white">
+                    <span>FAQ</span>
+                  </a>
+                </li>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={TERMS_HREF} className="text-white">
+                    <span>Terms of Service</span>
+                  </a>
+                </li>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={PRIVACY_HREF} className="text-white">
+                    <span>Privacy Policy</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-column">
+              <h3 className="text-light text-right">Socials</h3>
+              <ul className='flex-column justify-content-end' style={{paddingInlineStart: 0}}>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={TALENT_PROTOCOL_TWITTER} className="text-white">
+                    <span>Twitter</span>
+                  </a>
+                </li>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={TALENT_PROTOCOL_DISCORD} className="text-white">
+                    <span>Discord</span>
+                  </a>
+                </li>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={TALENT_PROTOCOL_GITHUB} className="text-white">
+                    <span>Github</span>
+                  </a>
+                </li>
+                <li className='mt-2 text-right'>
+                  <a target="_blank" rel="noreferrer" href={TALENT_PROTOCOL_TELEGRAM} className="text-white">
+                    <span>Telegram</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </footer>
       </main>
